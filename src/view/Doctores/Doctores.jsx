0e86750abe0004d77b51f770d2683251c4181181
kdiazc1 <kdiazc@unal.edu.co>
2024-05-01 17:34:00 -0500

    feat(): cambio doctoras

--- src/view/Doctores/Doctores.jsx
+++ src/view/Doctores/Doctores.jsx
@@ -6,7 +6,7 @@ import { FaTwitter } from "react-icons/fa";
 const Doctores = ()=>{
     return(
         <div className={styles.ContainerMain}>
-            <h1>Doctores</h1>
+            <h1>Doctoras</h1>
             <h3>Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</h3>
             <div className={styles.parent}>
 
